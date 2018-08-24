6d32a064665a094c62478a761ba10cb5b040e60e
o-sta <41351962+o-sta@users.noreply.github.com>
2018-08-24 17:10:02 +0900

    none

--- plot_data.m
+++ plot_data.m
@@ -14,10 +14,8 @@ close all;
 fig1 = figure(1);
 movegui('northwest')
 hold on
-
-
 plot(out.phi/(2*pi)*360,'LineWidth',2);
-plot(Lout.phi/(2*pi)*360,'--','LineWidth',2);
+plot(Ldata.Time,Ldata.Data(:,7)/(2*pi)*360,'--','LineWidth',2);
 set(gca,'FontName','arial','FontSize',14)
 xlabel('time [s]','FontName','arial','FontSize',16)
 ylabel('phi[rad/s]','FontName','arial','FontSize',16)
@@ -29,11 +27,18 @@ fig2 = figure(2);
 iptwindowalign(fig1,'right',fig2,'left');
 hold on
 plot(out.y,'LineWidth',2);
-plot(Lout.y,'--','LineWidth',2);
+plot(Ldata.Time,Ldata.Data(:,2),'--','LineWidth',2);
 set(gca,'FontName','arial','FontSize',14)
 xlabel('時間 [s]','FontName','arial','FontSize',16)
 ylabel('速度[m/s]','FontName','arial','FontSize',16)
 ylim manual%([-100 0]);
 legend('Nonlinear','Linear')
 set(legend,'FontName','arial','FontSize',16)
+
+fig3 = figure(3);
+plot(u_in);
+set(gca,'FontName','arial','FontSize',14)
+xlabel('時間 [s]','FontName','arial','FontSize',16)
+ylabel('信号[]','FontName','arial','FontSize',16)
+ylim manual%([-100 0]);
 
