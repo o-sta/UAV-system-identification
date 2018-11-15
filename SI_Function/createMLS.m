64f1808d702eca7b7b6e686a9d01be17f0ae5321
o-sta <41351962+o-sta@users.noreply.github.com>
2018-11-15 13:51:36 +0900

    M系列信号を生成する関数

--- SI_Function/createMLS.m
+++ SI_Function/createMLS.m
@@ -1,54 +1,94 @@
-function [u,outputArg2] = createMLS(n, mm)
+function [continuous_signal, discrete_signal, ts] = createMLS(number, seeds, min_value, max_value, length_of_time, continuous_signal_ts)
 %==========================================================================
 % ◎ createMLS()
+% 二値信号を生成します。
+% number段シフトレジスタを用いて、mix及びmaxのみを取るランダムな
+% 二値信号を生成します。length_of_timeで信号の長さを大まかに設定しますが、
+% サンプリング時間との兼ね合いにより多少変化することがあります。
+% 結果は連続時間信号のcontinuous_signalと離散時間信号のdiscrete_signal、
+% サンプリング周期(時間)のtsを返します。
 %==========================================================================
-% u = createMLS(n, mm)
-% n : n段シフトレジスタ
-% [min max] : 最小値 最大値 ※出力値を変更する場合
-% 
-while 1
-    x = randi([0 1], 5, 1); % 初期値作成
-    if ~all(x(:) == 0)      % 全てゼロのときはもう一回
-        break;
-    end
+% continuous_signal     二値連続時間信号
+% discrete_signal       二値離散時間信号
+% ts                    二値離散時間信号のサンプリング周期
+% number                number段シフトレジスタ（デフォルトは5）
+% seeds                 シード値 seeds mod 2^number を2進数で表したものがレジスタの状態xの初期値になるが、0になったときはランダムにシードが選択される。
+% min                   二値信号の最小値
+% max                   二値信号の最大値
+% length_of_time        信号全体の時間
+% continuous_signal_ts  連続時間信号に格納するデータのサンプリング時間
+
+% シフトレジスタの段数からフィードバックをどのレジスタの内容をフィードバックするか決める
+switch number
+case 2
+  feedback_register = 1;
+case 3
+  feedback_register = 1;
+case 4
+  feedback_register = 1;
+case 5
+  feedback_register = 2;
+case 6
+  feedback_register = 1;
+case 7
+  feedback_register = 1;
+case 9
+  feedback_register = 4;
+case 10
+  feedback_register = 3;
+case 11
+  feedback_register = 2;
+case 15
+  feedback_register = 1;
+otherwise
+  number = 5;
+  feedback_register = 2;
 end
 
+% レジスタの初期値を設定
+if mod(seeds, 2^number) == 0
+  while true
+    x = randi([0 1], number, 1)  % レジスタの状態xの初期値をランダムに決定
+    if ~sum(x)==0                 % レジスタの値が全て0であればやり直し
+      break;
+    end
+  end
+else
+  seeds_bin = seeds;
+  x = zeros(number,1)
+  for i = 1:number
+    x(i) = mod(seeds_bin,2);
+    seeds_bin = idivide(int32(seeds_bin),2);
+  end
+end
 
-
-
-% %--------------------------------------------------------------------------
-% outputArg1 = inputArg1
-% if exist('inputArg2') ~= 0
-%     outputArg2 = inputArg2
-% end
-% 
-% mP = 5;     % M系列のmP段シフトレジスタ
-% mk = 2;     % 最終段以外にフィードバックさせる信号
-% 
-% x = randi([0 1], mP, 1);    % 状態の初期値をランダムに決定
-% x = [0 1 0 0 1]';           % 状態の初期値を指定（全て0は禁止）
-% u = zeros(2^mP-1, 1);       % 出力変数の定義
-% for i = 1:2^mP-1            % 信号の生成
-%     x = [xor(x(mk),x(end));x(1:end-1)]; % フィードバック
-%     u(i) = x(1);
-% end
-% for i = 1:2^mP - 1          % 2値信号の値を0,1から他の値に変更
-%     if u(i) == 1
-%         u(i)=1;
-%     else
-%         u(i)=-1;
-%     end
-% end
-% s_t = 5;        % 信号全体の時間[s]
-% s_tp = 0.001;   % 離散信号1個あたりの継続時間[s] / サンプリング時間
-% s_p = round(s_t/s_tp/numel(u));               % 1個あたりの連続時間信号数
-% t_data(:,1) = 0:s_tp:s_tp*s_p*numel(u)-s_tp;  % 時間信号を作成
-% u_data = zeros(size(t_data));
-% for i = 1:numel(u)  % 信号を作成
-%     for j = (i-1)*s_p+1:i*s_p
-%         u_data(j) = u(i);
-%     end
-% end
+% 二値信号u[0 or 1]の作成　信号の数は 2^number-1 個
+discrete_signal = zeros(2^number-1, 1);   % 離散時間信号discrete_signalの定義
+for i = 1:2^number-1
+    x = [xor(x(feedback_register),x(end));x(1:end-1)]; % フィードバック
+    discrete_signal(i) = x(1);
+end
+% 2値信号の値を[0,1]から他の値に変更
+for i = 1:2^number - 1
+    if discrete_signal(i) == 1
+        discrete_signal(i) = max_value;
+    else
+        discrete_signal(i) = min_value;
+    end
+end
+% 連続時間信号に変換
+number_of_signal = round((length_of_time/continuous_signal_ts)/numel(discrete_signal)); % 離散時間信号1個に対応する連続時間信号のデータ数
+continuous_signal_times(:,1) = 0:continuous_signal_ts:continuous_signal_ts*number_of_signal*numel(discrete_signal)-continuous_signal_ts;  % 連続時間信号のタイムスタンプ作成
+continuous_signal_data = zeros(size(continuous_signal_times));
+for i = 1:numel(discrete_signal)  % 連続時間信号のデータを作成
+    for j = (i-1)*number_of_signal+1:i*number_of_signal
+        continuous_signal_data(j) = discrete_signal(i);
+    end
+end
+continuous_signal = timeseries(continuous_signal_data, continuous_signal_times, 'name', 'Position');  % 時系列作成
+ts = number_of_signal * continuous_signal_ts; % 周期の算出
+% pst = xcorr(continuous_signal_data,continuous_signal_data)
+% plot(pst)
 
 end
 
